db7668c09b28e0a0ef6dd4bbf374056fe1b8cd28
NULL-BYTE <sonaligupta9802@gmail.com>
2020-09-23 18:36:17 +0530

    final touch

--- script.js
+++ script.js
@@ -4,24 +4,28 @@ const quizData = [
         a: 'West-Bengal',
         b: 'Tamil Nadu',
         c: 'Liquid',
-        d: 'Solid'
+        d: 'Solid',
+        correct: 'c'
     },
     {
         question: 'What is in the  middle of paris?',
         a: 'Noter Dame',
         b: "Letter 'R'",
         c: 'Moulin Rouge',
-        d: 'Bourgogne'
+        d: 'Bourgogne',
+        correct: "b"
     },
     {
         question: 'Which of the following table you can eat?',
         a: 'Dining Table',
         b: 'Computer Table',
         c: 'Vegetable',
-        d: 'None of these'
+        d: 'None of these',
+        correct: "c"
     },
 ];
-
+const answerEl = document.querySelectorAll(".answer");
+const quiz = document.getElementById("quiz");
 const questionEl =document.getElementById("question");
 const a_text = document.getElementById("a_text");
 const b_text = document.getElementById("b_text");
@@ -31,12 +35,15 @@ const submit = document.getElementById("submit-ans");
 
 
 let currentQuiz = 0;
+//let answer = undefined;
+let score = 0;
 
 loadQuiz();
 
 
 function loadQuiz()
 {
+    deSelectAnswers()
     const currentQuizData = quizData[currentQuiz]
 
     questionEl.innerText = currentQuizData.question;
@@ -47,8 +54,51 @@ function loadQuiz()
 
 }
 
-submit.addEventListener('click', () => {
-    currentQuiz++;
+function getSelected() 
+{
+    let answer = undefined;
+    answerEl.forEach((answerEl) => {
+        if(answerEl.checked) {
+            answer = answerEl.id;
+        }
+    });
+    return answer;
+}
 
-    loadQuiz();
+function deSelectAnswers()
+{
+    answerEl.forEach((answerEl) => {
+        answerEl.checked =false;    })
+}
+
+submit.addEventListener('click', () => {
+
+    const answer = getSelected();
+    if(answer)
+    {
+        if(answer === quizData[currentQuiz ].correct)
+        {
+            score++;
+        }    
+        currentQuiz++;
+        if(currentQuiz < quizData.length) 
+        {
+            loadQuiz();
+        }
+        else
+        {
+            document.getElementById("quiz  ").innerHTML = "You scored " + score + " out of " + quizData.length;
+            //document.getElementById("try").innerHTML = "realoding..";
+            //setInterval(function(){ 
+              //  location.reload();
+             //}, 3000);
+            
+        }
+        
+    }
 });
+
+function sample()
+{
+    location.reload();
+}
